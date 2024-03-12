1b429ed696a2b2a6fbf20a88245bd0f05e32cf6e
Konstantin Denerz <konstantin.denerz@gmail.com>
2024-03-12 08:13:54 +0100

    update

---
+++ docs/assets/channel-d3ac35ac-BsjY8xct.js
@@ -0,0 +1,1 @@
+import{aQ as o,aR as r}from"./slidev/6-Bc_CyDrk.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
--- docs/assets/channel-d3ac35ac-Bxyv7xzL.js
+++
@@ -1,1 +0,0 @@
-import{aQ as o,aR as r}from"./slidev/6-rCByBM2s.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
